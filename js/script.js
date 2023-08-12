cb09fa63a232961347d85088fb27fd608b900b62
Viktor <ololoshe4kaaaaaa@gmail.com>
2023-08-12 13:27:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,118 @@
+// 'use strict';
+
+// // const arr = [1, 2, 3];
+// // console.log(arr[1]);
+
+
+// // const arrObj = {
+// //     a: 'a',
+// //     '1': 'b',
+// //     2: 'c'
+// // }
+
+
+// // console.log(arrObj[1]);
+
+
+
+
+
+
+// // // const obj = {a: 1, b: 2};
+
+// // const obj = {
+// //     'anna': 500,
+// //     'alice': 800
+// // };
+
+  
+
+// // alert('Hello');
+
+// // const result = confirm('a u here');
+// // console.log(result);
+
+// // const answer = +prompt('How old a u?', '');
+// // console.log(answer + 5);
+
+// // const answers = [];
+
+// // answers[0] = prompt('W is this', '');
+// // answers[1] = prompt('W are y mother name', '');
+// // answers[2] = prompt('W are y ', '');
+// // answers[3] = prompt('W is y name', '');
+
+// // console.log(typeof(answers));
+
+
+// // const category = 'toys';
+
+// // console.log(`http://someurl.com/${category}/5`);
+
+// // const user = 'Ivan';
+
+// // alert(`Hi, ${user}`);
+
+
+
+// // console.log('arr' + "- object");
+// // console.log(4 + " 5");
+
+// // let incr = 10,
+// //     decr = 10;
+
+// // // incr++;
+// // // decr--;
+
+// // console.log(incr++);
+// // console.log(decr--);
+
+
+// // console.log(5%2);
+
+// // console.log(2*4 === 8);
+
+
+
+
+
+// // console.log(5%2);
+
+// // console.log(2 + 2 * 2 !== '6');
+
+
+
+
+
+// //   &&    означає  і
+
+// //    ||             або
+
+
+// $
+
+// // const isChecked = true,
+// //       isClose   = true;
+
+
+// // console.log(isChecked && isClose);
+
+
+
+// // const isChecked = true,
+// //       isClose   = false;
+
+
+// // console.log(isChecked || isClose);
+
+
+
+// // const isChecked = true,
+// //       isClose   = true;
+
+
+// // console.log(isChecked && !isClose);
+
+
+
+
